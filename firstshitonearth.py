4ece87876f6477c0dc6fa15643fd03d763bb710d
Veronika Shatunova <veronika.sshatunova@gmail.com>
2024-08-22 21:19:38 +0300

    блять помогите это ужасно ничего не получается

--- firstshitonearth.py
+++ firstshitonearth.py
@@ -1,6 +1,26 @@
-print("простое вычисление держите")
-print("введите количество красных рыб")
-x = int(input())
-print("введите количество голубых рыб")
-y = int(input())
-print ("пожалуйста,", x + y, "рыбов получаете" )
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+BOT_TOKEN = '7541959279:AAEeMOQTNGeX2tVJM1MayBsvNKtmWGyD03M'
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nЭто бля многострадальный ботяра\nПишите')
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
